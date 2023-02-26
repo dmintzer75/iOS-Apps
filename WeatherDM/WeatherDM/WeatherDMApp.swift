b1b9b1d353d5e703a7abcea6c5678b298a6e22a1
Dario Mintzer <dario.mintzer.ext@kavak.com>
2023-02-26 10:49:49 -0300

    first commit

---
+++ WeatherDM/WeatherDM/WeatherDMApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDMApp.swift
+//  WeatherDM
+//
+//  Created by Dario Mintzer on 26/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
